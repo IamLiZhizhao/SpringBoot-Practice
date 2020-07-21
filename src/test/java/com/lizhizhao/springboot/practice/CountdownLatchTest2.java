4809fcd44605c4a3403ee85d78e9f4b24d1421bb
李志钊 <lizhizhaooo@163.com>
2020-07-21 12:03:32 +0800

    [CountDownLatch]

---
+++ src/test/java/com/lizhizhao/springboot/practice/CountdownLatchTest2.java
@@ -0,0 +1,58 @@
+package com.lizhizhao.springboot.practice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CountdownLatchTest2 {
+
+    /**
+     * 百米赛跑，4名运动员选手到达场地等待裁判口令，裁判一声口令，选手听到后同时起跑，当所有选手到达终点，裁判进行汇总排名
+     * 做法是初始化一个共享的CountDownLatch(1)，将其计算器初始化为1，多个线程在开始执行任务前首先countdownlatch.await()，
+     * 当主线程调用countDown()时，计数器变为0，多个线程同时被唤醒。
+     */
+    @Test
+    public void runExam() {
+        ExecutorService service = Executors.newCachedThreadPool();
+        final CountDownLatch cdOrder = new CountDownLatch(1);
+        final CountDownLatch cdAnswer = new CountDownLatch(4);
+        for (int i = 0; i < 4; i++) {
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("选手" + Thread.currentThread().getName() + "正在等待裁判发布口令");
+                        cdOrder.await();
+                        System.out.println("选手" + Thread.currentThread().getName() + "已接受裁判口令");
+                        Thread.sleep((long) (Math.random() * 10000));
+                        System.out.println("选手" + Thread.currentThread().getName() + "到达终点");
+                        cdAnswer.countDown();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            service.execute(runnable);
+        }
+        try {
+            Thread.sleep((long) (Math.random() * 10000));
+            System.out.println("裁判"+Thread.currentThread().getName()+"即将发布口令");
+            cdOrder.countDown();
+            System.out.println("裁判"+Thread.currentThread().getName()+"已发送口令，正在等待所有选手到达终点");
+            cdAnswer.await();
+            System.out.println("所有选手都到达终点");
+            System.out.println("裁判"+Thread.currentThread().getName()+"汇总成绩排名");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        service.shutdown();
+    }
+
+}
