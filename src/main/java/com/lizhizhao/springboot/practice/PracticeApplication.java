53a10a964a57095aa5692531a75b6eda640a63cd
李志钊 <lizhizhaooo@163.com>
2020-11-06 15:34:06 +0800

    feat: kafka

--- src/main/java/com/lizhizhao/springboot/practice/PracticeApplication.java
+++ src/main/java/com/lizhizhao/springboot/practice/PracticeApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PracticeApplication {
 
 	public static void main(String[] args) {
