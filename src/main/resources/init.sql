64af602da642b88d5955d690d0951c99ce106807
lizhizhao <lizhizhaooo@163.com>
2023-07-22 15:24:22 +0800

    feat：补全初始SQL

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -9,3 +9,17 @@ CREATE TABLE `sys_log` (
   `create_time` varchar(64) DEFAULT NULL COMMENT 'IP地址',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE `student` (
+  `sno` varchar(50) NOT NULL,
+  `sname` varchar(255) DEFAULT NULL,
+  `ssex` varchar(32) DEFAULT NULL,
+  `score` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`sno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO student VALUES ('001', 'KangKang', 'M ', 88.5);
+INSERT INTO student VALUES ('002', 'Mike', 'M ', 98.5);
+INSERT INTO student VALUES ('003', 'Jane', 'F ', 86.5);
